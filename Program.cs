ace24dc88f786b39683f6963b21929c2dfd687cd
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-25 08:57:24 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvjeraZnanjaZadataka5
+{
+    class NebeskoTijelo
+    {
+        double brzinaOkoOsi, brzinaOkoSunca;
+        public double BrzinaOkoOsi { get => brzinaOkoOsi; set => brzinaOkoOsi = value; }
+        public double BrzinaOkoSunca { get => brzinaOkoSunca; set => brzinaOkoSunca = value; }
+    }
+    class Planet : NebeskoTijelo
+    {
+        public Planet(double o, double s)
+        {
+            BrzinaOkoOsi = o;
+            BrzinaOkoSunca = s;
+        }
+    }
+    class Satelit : NebeskoTijelo
+    {
+        public Satelit(double o, double s)
+        {
+            BrzinaOkoOsi = o;
+            BrzinaOkoSunca = s;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Planet Zemlja = new Planet(55.3, 26.5);
+            Satelit Mjesec = new Satelit(35.3, 26.25);
+            Console.ReadLine();
+        }
+    }
+}
